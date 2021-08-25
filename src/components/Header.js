c855b5dd565790c425007da1503d9544a68ee970
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-25 23:00:32 +0600

    Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            <nav className="nav">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/cat">Cat</Link>
+                <Link className="nav-link" to="/pokemon">Pokemon</Link>
+                <Link className="nav-link" to="/pokemongame">PokeGame</Link>
+                <Link className="nav-link" to="/starwars">StarWars</Link>
+                <Link className="nav-link" to="/people">People</Link>
+                <Link className="nav-link" to="/users">Users</Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
